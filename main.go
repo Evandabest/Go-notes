c22abf996ca9b997cc381c1d9ccf4d887daca363
Evandabest <evanhaque1@gmail.com>
2025-02-11 22:44:26 -0500

    updates to coding part

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
+// first go file yay
+package main
 
+import (
+	"fmt"
+	t "time"
+)
 
-//first go file yay
+func main() {
+	fmt.Println("hello world")
+	name := "evan"
+
+	num := 19
+
+	fact := false
+
+	//fmt.Printf("My name is %q", num)
+	fmt.Printf("My name is %s and I am %d years old. Fact or cap? %t", name, num, fact)
+
+	fmt.Println("The time right now is", t.Now())
+	var number uint16 = 2
+	fmt.Println(number)
+}
